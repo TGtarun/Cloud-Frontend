1962b68b0be56f4a4a45217258b04c71c27db243
Tarun Goyal <goyaltarun2002@gmail.com>
2021-04-17 01:24:44 +0530

    init

---
+++ client/src/components/displays/GetRequest.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import axios from "axios";
+import UserProvider from "../../contexts/UserProvider";
+import _ from "lodash";
+const fs = require("fs");
+
+// const userData = useContext(UserProvider.context);
+// const {id, name} =  userData;
+
+class GetRequest extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalBalance: 0,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      // .get("https://api.npms.io/v2/search?q=react")
+      .get(
+        `https://piggybankbackend.mybluemix.net/getBalance/113403933642326679347`
+      )
+      .then((response) => this.setState({ totalBalance: response }));
+  }
+
+  render() {
+    const { totalBalance } = this.state;
+
+    return (
+      <div>
+        <div className="form-content-right">
+          <h1>Your account balance is</h1>
+          <button className="form-input-btn-balance">₹ {totalBalance}</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GetRequest;
